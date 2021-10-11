b3ffde1c2acc53034ad60f74f68847734c50dcae
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-10-11 11:48:42 -0500

    monday AM

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,28 @@
-const http = require('http')
+const express = require('express')
+const app = express() // invoke it
 
-const server = http.createServer((req,res)=>{
-  console.log('user hit the server')
-  res.end('home page')
+// invoked everytime user is performing GET request on domain
+app.get('/',(req, res)=>{
+  console.log('user hit the resource')
+  res.status(200).send('Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req,res)=>{
+  res.status(200).send('About Page')
+})
+
+app.all('*',(req,res)=>{
+  res.status(404).send('<h1>resource not found</h1>')
+})
+
+app.listen(5000,()=>{
+  console.log('server is listening on port 5000...')
+})
+
+// app.get - read data
+// app.post - insert data
+// app.put - update data
+// app.delete - delete data
+// app.all - apply to all HTTP verbs
+// app.use - middleware
+// app.listen
