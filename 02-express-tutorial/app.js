bcd7ae771a562e44c1d7c4c7e01366770102112c
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-10-21 15:59:29 -0500

    Thursday PM

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -21,6 +21,14 @@ app.post('/api/people',(req,res)=>{
   res.status(201).json({success:true,person:name})
 })
 
+app.post('/api/postman/people',(req,res)=>{
+  const {name} = req.body
+  if (!name) {
+    return res.status(400).json({success:false,msg:'please provide name value'})
+  }
+  res.status(201).json({success:true,data:[...people, name]})
+})
+
 app.post('/login',(req,res)=>{
   const {name} = req.body
   if (name) {
@@ -28,6 +36,37 @@ app.post('/login',(req,res)=>{
   }
 
   res.status(401).send('Please Provide Credentials')
+})
+
+app.put('/api/people/:id', (req,res)=>{
+  const {id} = req.params
+  const {name} = req.body
+
+  const person = people.find((person)=> person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({success:false,msg:`no person with ${id}`})
+  }
+  const newPeople = people.map((person)=>{
+    if(person.id === Number(id)){
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({success:true,data:newPeople})
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+  const person = people.find((person)=> person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({success:false,msg:`no person with ${req.params.id}`})
+  }
+  const newPeople = people.filter((person)=> person.id !== Number(req.params.id) )
+  return res.status(200).json({success:true, data: newPeople})
 })
 
 app.listen(5000,()=>{
